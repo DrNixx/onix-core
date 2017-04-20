50bb1072bcee682723f5b0840403244cdfc2d096
DrNixx <drnix@chess-online.com>
2017-04-20 16:58:11 +0300

    Add IUser interface

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ export * from './fn/Types';
 export * from './fn/array/index';
 export * from './fn/string/index';
 export * from './fn/number/index';
+
+export * from './app/index';
+
 export { Hashtable } from './Hashtable';
 export { Logger } from './Logger';
 export { Intl } from './Intl';
