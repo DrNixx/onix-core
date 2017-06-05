56bb1a857a97e44f79aed23e6426092aa0dfd630
Nick Panteleeff <drnix@chess-online.com>
2017-06-05 16:09:01 +0300

    Update character case in filename

--- src/fn/number/index.ts
+++ src/fn/number/index.ts
@@ -1,6 +1,6 @@
 export { intVal } from './IntVal';
 export { isValueOutOfRange } from './IsValueOutOfRange';
-export { ensureValueInRange } from './ensureValueInRange';
+export { ensureValueInRange } from './EnsureValueInRange';
 
 export { average } from './Average';
 export { stdDeviation } from './StdDeviation';
