61bf1ba44aafb77dc4526ba7212f53959e040040
Nick Panteleeff <drnix@chess-online.com>
2017-07-25 17:04:30 +0300

    Add date functions

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export * from './fn/Types';
 export * from './fn/array/index';
 export * from './fn/string/index';
 export * from './fn/number/index';
+export * from './fn/date/index';
 
 export * from './fn/utils/index';
 
